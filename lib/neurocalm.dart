e63730c75ce45460e8c81e72eaddf8b1216058f5
ferdoushasanpr <ferdoushasanpr@gmail.com>
2025-12-29 15:19:59 +0600

    feat: Initialize Project with Home Screen

---
+++ lib/neurocalm.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:neurocalm/screens/login.dart';
+
+class Neurocalm extends StatelessWidget {
+  const Neurocalm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset('assets/images/welcome-page.png'),
+          SizedBox(height: 32),
+          Container(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    'We are what we do',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(63, 65, 78, 1),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(48, 0, 48, 26),
+                  child: Text(
+                    'Thousand of people are usign silent moon for smalls meditation.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color.fromRGBO(63, 65, 78, 1),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginScreen();
+                        },
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(142, 151, 253, 1),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 48.0,
+                      vertical: 18.0,
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(color: Color.fromRGBO(246, 241, 251, 1)),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "ALREADY HAVE AN ACCOUNT? LOG IN",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color.fromRGBO(63, 65, 78, 1),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
